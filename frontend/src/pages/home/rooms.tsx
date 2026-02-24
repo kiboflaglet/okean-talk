0a7382b7fd0a7338b7fd1fd68df6659b621cf504
kiboflaglet <kiboflaglet@gmail.com>
2026-02-24 17:52:12 +0400

    hotFix/redirecting-to-localhost-in-production & loading UI added for rooms

--- frontend/src/pages/home/rooms.tsx
+++ frontend/src/pages/home/rooms.tsx
@@ -4,24 +4,39 @@ import { useRoomsContext } from "../../provider/roomsContext";
 import type { HomeLoader } from "../../types";
 import RoomSingle from "./room-single";
 import { SignInDialog } from "./sign-in-dialog";
+import { Loader } from "lucide-react";
 
 const Rooms = () => {
-  const { rooms } = useRoomsContext();
+  const { rooms, loading } = useRoomsContext();
 
   const loaderData: HomeLoader = useLoaderData();
 
   const [signInOpen, setSignInOpen] = useState(false);
 
+
   return (
     <section className="grid grid-cols-1 gap-x-15 gap-y-8  pb-10 md:grid-cols-2 lg:grid-cols-3 mt-6">
-      {rooms.length <= 0 &&  <span className="text-gray-10">No room found, be the first one, click on + icon</span>}
+      {loading && (
+        <span className="text-gray-10 flex items-center gap-2 ">
+          <Loader className="animate-spin w-4 h-4" /> Loading
+        </span>
+      )}
+      {!loading && rooms.length <= 0 && (
+        <span className="text-gray-10">
+          No room found, be the first one, click on + icon
+        </span>
+      )}
+
       {rooms.map((item) => (
         <RoomSingle
           joinRoom={() => {
             if (!loaderData?.userData) {
               setSignInOpen(true);
             } else {
-              window.open(`http://localhost:5173/room/${item.id}`, "_blank");
+              window.open(
+                `${window.location.origin}/room/${item.id}`,
+                "_blank"
+              );
             }
           }}
           key={item.id}
